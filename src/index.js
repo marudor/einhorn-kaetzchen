9642981804645bb619401454509fb85c0ab39c18
marudor <marudor@marudor.de>
2015-01-22 19:22:46 +0100

    streaming

--- src/index.js
+++ src/index.js
@@ -26,15 +26,38 @@ if (last) {
   options.since_id = last;
 }
 
-client.get('statuses/user_timeline', options, function(error, params){
+function processTweet(tweet) {
+  if (tweet.entities.media.length > 0) {
+    _.each(tweet.entities.media, m => {
+      new download().get(m.media_url_https).rename(moment(tweet.created_at).format('DD.MM.YYYY HHmm')).dest('episodes').run();
+    });
+  }
+}
+
+client.get('statuses/user_timeline', options, (error, params) => {
   _.each(params.reverse(), tweet => {
     last = tweet.id;
-    if (tweet.entities.media.length > 0) {
-      _.each(tweet.entities.media, m => {
-        new download().get(m.media_url_https).rename(moment(tweet.created_at).format('DD.MM.YYYY HHmm')).dest('episodes').run();
-      });
-    }
+    processTweet(tweet);
   });
   last+=100;
   fs.writeFileSync(__dirname+'/last.json', last);
 });
+
+var startStream = () => {
+  client.stream('statuses/filter', {track: 'javascript'}, stream => {
+
+    stream.on('data', tweet => {
+      processTweet(tweet);
+      if (last < tweet.id) {
+        last = tweet.id + 100;
+        fs.writeFileSync(__dirname+'/last.json', last);
+      }
+    });
+
+    stream.on('error', () => {
+      setTimeout(() => {
+        startStream();
+      });
+    });
+  });
+};
