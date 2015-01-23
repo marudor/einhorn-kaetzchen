e0e9c60e63833625f46cd90c8b52e48f8ece762e
marudor <marudor@marudor.de>
2015-01-23 20:01:07 +0100

    include old

--- src/index.js
+++ src/index.js
@@ -15,6 +15,11 @@ try {
   last = require('./last.json');
 } catch (e) {}
 
+var gotOld;
+try {
+  gotOld = require('./gotOld.json');
+} catch (e) {}
+
 
 var options = {
   user_id: 2978671564,
@@ -26,12 +31,39 @@ if (last) {
   options.since_id = last;
 }
 
+
 function processTweet(tweet) {
   if (tweet.entities.media.length > 0) {
     _.each(tweet.entities.media, m => {
       new download().get(m.media_url_https+':large').rename(moment(tweet.created_at).format('DD.MM.YYYY HHmm')+'.png').dest('episodes').run();
     });
   }
+}
+
+var max;
+var findOld = function(max_id = 556942909426389000) {
+  client.get('statuses/user_timeline', {
+    screen_name: 'sweet_sacura',
+    count: 200,
+    max_id: max_id,
+    exclude_replies: true,
+    include_rts: false
+  }, (error, params) => {
+    _.each(params, tweet => {
+      max = tweet.id;
+      if ((tweet.text.indexOf('Einhornkätzchen') !== -1 || tweet.text.indexOf('einhornkätzchen') !== -1) && tweet.entities.media && tweet.entities.media.length > 0) {
+        processTweet(tweet);
+      }
+    });
+    if (params && params.length > 0) {
+      findOld(max);
+    }
+  });
+};
+
+if (!gotOld) {
+  findOld();
+  fs.writeFileSync(__dirname+'/max.json', max);
 }
 
 client.get('statuses/user_timeline', options, (error, params) => {
@@ -47,6 +79,9 @@ var startStream = () => {
   client.stream('statuses/filter', {follow: 2978671564}, stream => {
 
     stream.on('data', tweet => {
+      if (tweet.retweeted) {
+        return;
+      }
       processTweet(tweet);
       if (last < tweet.id) {
         last = tweet.id + 100;
